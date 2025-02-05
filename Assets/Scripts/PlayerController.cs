913aadfd0860a3844730de99fbcc16e5fb00ac73
ZeroTKA <zerotka@gmail.com>
2025-02-04 20:24:32 -0700

    Code Review
    Did a quick clean up and added some notes / to do list.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
 
+
+//-- To Do List --//
+// 1. Jumping
+// 2. Mouse Look
+// 3. Sprinting
+// 4. Specific for Hotkeys?? Do I need to even do that?
+
+
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    CharacterController playerCon;
+    [SerializeField] private float moveSpeed;
+    CharacterController characterController;
     private float gravity = 9.8f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerCon = GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<CharacterController>();
+        characterController = GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -17,14 +25,18 @@ public class PlayerController : MonoBehaviour
         PlayerMovement();
     }
 
+
+    //-- Main Functions --//
     void PlayerMovement()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        playerCon.Move(move * Time.deltaTime * speed);
+        characterController.Move(move * Time.deltaTime * moveSpeed);
         Gravity();
     }
+
+    //-- Sub Functions --//
     void Gravity()
     {
-        playerCon.Move(Vector3.down * Time.deltaTime * gravity);
+        characterController.Move(Vector3.down * Time.deltaTime * gravity);
     }
 }
